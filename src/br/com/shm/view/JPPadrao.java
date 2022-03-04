f24055a49c75edc0d1f778c7771754c94dec5ed6
Flaigor <igorhuayck@hotmail.com>
2022-03-04 15:57:13 -0300

    JPanel base

---
+++ src/br/com/shm/view/JPPadrao.java
@@ -0,0 +1,28 @@
+package br.com.shm.view;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import javax.swing.JComboBox;
+import javax.swing.JPanel;
+
+public abstract class JPPadrao extends JPanel {
+	
+	protected Integer width = 1500;
+	protected Integer height = 750;
+
+	public void limpaTela( )
+	{
+		revalidate( );
+		repaint( );
+		setLayout( null );
+		setBackground( new Color( 100, 100, 100 ) );
+	}
+	
+	public void finaliza( )
+	{
+		
+	}
+}
