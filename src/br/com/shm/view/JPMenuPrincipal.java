dc6cf6b4920b700cfb836e0debdcf4be40b2d1d9
Flaigor <igorhuayck@hotmail.com>
2022-03-18 06:02:03 -0300

    Att no Banco e Ajustes do JPMenuPrincipal

--- src/br/com/shm/view/JPMenuPrincipal.java
+++ src/br/com/shm/view/JPMenuPrincipal.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JScrollPane;
 
 public class JPMenuPrincipal extends JPPadrao {
 
@@ -25,24 +26,36 @@ public class JPMenuPrincipal extends JPPadrao {
 		JButton btnPoduto = new JButton("Produtos");
 		btnPoduto.setBounds( 10 , 50 , 120, 30 );
 		
-		JButton btnVenda = new JButton("Vendas");
-		btnVenda.setBounds( 10 , 90 , 120, 30 );
+		JButton btnHistorico = new JButton("Histórico");
+		btnHistorico.setBounds( 140 , height - 80 , width - 300, 30 );
 		
 		JButton btnlogOff = new JButton("Logoff");
 		btnlogOff.setBounds( width - 150 , 10 , 120, 30 );
 		
-		JButton btnMudarSenha = new JButton("Mudar Senha");
-		btnMudarSenha.setBounds( width - 150 , 50 , 120, 30 );
+		JButton btnUsers = new JButton("Usuários");
+		btnUsers.setBounds( width - 150 , 50 , 120, 30 );
 		
 		JButton btnNovaVenda = new JButton("Nova Venda!");
 		btnNovaVenda.setBounds( 140 , 10 , width - 300, 70 );
+		
+		JButton btnVenda = new JButton("Venda");
+		btnVenda.setBounds( 10 , height - 80 , 120, 30 );
+		
+		JButton btnAgradecimentos = new JButton("Menções");
+		btnAgradecimentos.setBounds( width - 150 , height - 80 , 120, 30 );
+		
+		JScrollPane scrollClientes = new JScrollPane();
+		scrollClientes.setBounds( 10 , 90 , width - 40, height - 180 );
 		
 		add(btnCliente);
 		add(btnPoduto);
 		add(btnVenda);
 		add(btnlogOff);
-		add(btnMudarSenha);
+		add(btnUsers);
 		add(btnNovaVenda);
+		add(btnHistorico);
+		add(btnAgradecimentos);
+		add(scrollClientes);
 		
 		frame.repaint();
 		
