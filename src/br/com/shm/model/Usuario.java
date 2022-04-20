68f246f54d3024310d29e04ee572340dc1d49fda
Flaigor <igorhuayck@hotmail.com>
2022-04-20 19:51:19 -0300

    Usuario feito

--- src/br/com/shm/model/Usuario.java
+++ src/br/com/shm/model/Usuario.java
@@ -2,4 +2,52 @@ package br.com.shm.model;
 
 public class Usuario {
 
+	private Integer id;
+	private String login;
+	private String senha;
+	private boolean admin;
+	
+	public Usuario() {
+		
+	}
+	
+	public Usuario(Integer id, String login, String senha, boolean admin) {
+		this.id = id;
+		this.login = login;
+		this.senha = senha;
+		this.admin = admin;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public boolean isAdmin() {
+		return admin;
+	}
+
+	public void setAdmin(boolean admin) {
+		this.admin = admin;
+	}
+	
 }
