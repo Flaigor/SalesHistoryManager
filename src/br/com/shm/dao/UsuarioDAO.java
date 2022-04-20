e3c6755758499f8b59c1a39a02d0399384dd63bb
Flaigor <igorhuayck@hotmail.com>
2022-04-20 19:49:48 -0300

    UserDAO completo

--- src/br/com/shm/dao/UsuarioDAO.java
+++ src/br/com/shm/dao/UsuarioDAO.java
@@ -1,8 +1,17 @@
 package br.com.shm.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JOptionPane;
 
 import br.com.shm.jdbc.ConnectionFactory;
+import br.com.shm.model.Cliente;
+import br.com.shm.model.Usuario;
 
 public class UsuarioDAO {
 
@@ -10,5 +19,154 @@ public class UsuarioDAO {
 	
 	public UsuarioDAO(){
 		this.con = new ConnectionFactory().getConnection();
+	}
+	
+	public void cadastrarUsuario(Usuario user) {
+		try
+		{
+			String sql = "Insert Into shmdb.users(NameUser, PasswordUser, AdminUser) "
+					+ "Values (?, ?, ?)";
+			
+			PreparedStatement stmt = con.prepareStatement(sql);
+			stmt.setString(1, user.getLogin());
+			stmt.setString(2, user.getSenha());
+			stmt.setBoolean(3, user.isAdmin());
+			
+			stmt.execute();
+			stmt.close();
+			
+			JOptionPane.showMessageDialog(null, "Usuário cadastrado com sucesso!");
+			
+		} catch(SQLException erro)
+		{
+			JOptionPane.showMessageDialog(null, "Falha no cadastro do Usuário, erro: " + erro);
+		}
+	}
+	
+	public void alterarUsuario(Usuario user) {
+		try
+		{
+			String sql = "Update shmdb.users set NameUser = ?, PasswordUser = ?, AdminUser = ? "
+					+ "Where IdUser = ?";
+			
+			PreparedStatement stmt = con.prepareStatement(sql);
+			stmt.setString(1, user.getLogin());
+			stmt.setString(2, user.getSenha());
+			stmt.setBoolean(3, user.isAdmin());
+			stmt.setString(4, user.getId().toString());
+			
+			stmt.execute();
+			stmt.close();
+			
+			JOptionPane.showMessageDialog(null, "Usuário alterado com sucesso!");
+			
+		} catch(SQLException erro)
+		{
+			JOptionPane.showMessageDialog(null, "Falha na alteração do Usuário, erro: " + erro);
+		}
+	}
+	
+	public void excluirUsuario(Usuario user) {
+		try
+		{
+			String sql = "Delete From shmdb.users Where IdUser = ?";
+			
+			PreparedStatement stmt = con.prepareStatement(sql);
+			stmt.setString(1, user.getId().toString());
+			
+			stmt.execute();
+			stmt.close();
+			
+			JOptionPane.showMessageDialog(null, "Usuário excluído com sucesso!");
+			
+		} catch(SQLException erro)
+		{
+			JOptionPane.showMessageDialog(null, "Falha na exclusão do Usuário, erro: " + erro);
+		}
+	}
+	
+	public List<Usuario> listarUsuario() {
+		try
+		{
+			List<Usuario> lista = new ArrayList<>();
+			String sql = "SELECT * FROM shmdb.users";
+			PreparedStatement stmt = con.prepareStatement(sql);
+			ResultSet rs = stmt.executeQuery();
+			
+			while(rs.next())
+			{
+				Usuario user = new Usuario();
+				user.setId(rs.getInt("IdUser"));
+				user.setLogin(rs.getString("NameUser"));
+				user.setSenha(rs.getString("PasswordUser"));
+				user.setAdmin(rs.getBoolean("AdminUser"));
+				
+				lista.add(user);
+			}
+			
+			return lista;
+		} catch(SQLException erro)
+		{
+			JOptionPane.showMessageDialog(null, "Falha em listar os Usuário, erro: " + erro);
+			return null;
+		}
+	}
+	
+	public boolean validaUsuario(String loign, String senha) {
+		try
+		{
+			String sql = "Select count(*) NumUser From shmdb.users where NameUser = ? and "
+					+ "PasswordUser = ?";
+			PreparedStatement stmt = con.prepareStatement(sql);
+			
+			stmt.setString(1, loign);
+			stmt.setString(2, senha);
+			
+			ResultSet rs = stmt.executeQuery();
+			
+			while(rs.next())
+			{
+				if(rs.getInt("NumUser") == 1)
+				{
+					return true;
+				}
+			}
+			
+			return false;
+		} catch(SQLException erro)
+		{
+			JOptionPane.showMessageDialog(null, "Falha em válidar o Usuário, erro: " + erro);
+			return false;
+		}
+	}
+	
+	public Usuario getUsuario(String loign, String senha) {
+		try
+		{
+			String sql = "Select * From shmdb.users where NameUser = ? and "
+					+ "PasswordUser = ?";
+			PreparedStatement stmt = con.prepareStatement(sql);
+			
+			stmt.setString(1, loign);
+			stmt.setString(2, senha);
+			
+			ResultSet rs = stmt.executeQuery();
+			
+			while(rs.next())
+			{
+				Usuario user = new Usuario();
+				user.setId(rs.getInt("IdUser"));
+				user.setLogin(rs.getString("NameUser"));
+				user.setSenha(rs.getString("PasswordUser"));
+				user.setAdmin(rs.getBoolean("AdminUser"));
+				return user;
+			}
+			
+			return null;
+		} catch(SQLException erro)
+		{
+			JOptionPane.showMessageDialog(null, "Falha em achar o Usuário, erro: " + erro);
+			return null;
+		}
 	}
 }
